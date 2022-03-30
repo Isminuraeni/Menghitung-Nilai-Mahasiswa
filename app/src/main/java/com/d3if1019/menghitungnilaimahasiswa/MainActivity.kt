ecf9afee29da7c469ce0c3f6ca90e29f29b875e3
Ismi_Nuraeni_Januarita <isminuraaeni@gmail.com>
2022-03-30 13:12:12 +0700

    Menambahkan view binding

--- app/src/main/java/com/d3if1019/menghitungnilaimahasiswa/MainActivity.kt
+++ app/src/main/java/com/d3if1019/menghitungnilaimahasiswa/MainActivity.kt
@@ -2,10 +2,23 @@ package com.d3if1019.menghitungnilaimahasiswa
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.d3if1019.menghitungnilaimahasiswa.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonHitung.setOnClickListener{ hitungNilai() }
+    }
+
+    private fun hitungNilai() {
+        Log.d("MainActivity", "Tombol diklik!")
     }
 }
