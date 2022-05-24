7eff59348c4a9fb7a07e78b5a3ef75259ddb9e6b
Ismi_Nuraeni_Januarita <isminuraaeni@gmail.com>
2022-05-24 16:51:33 +0700

    menyimpan preferences menggunakan DataStore

--- app/src/main/java/com/d3if1019/menghitungnilaimahasiswa/ui/histori/HistoriFragment.kt
+++ app/src/main/java/com/d3if1019/menghitungnilaimahasiswa/ui/histori/HistoriFragment.kt
@@ -2,15 +2,22 @@ package com.d3if1019.menghitungnilaimahasiswa.ui.histori
 
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.d3if1019.menghitungnilaimahasiswa.R
+import com.d3if1019.menghitungnilaimahasiswa.data.SettingDataStore
+import com.d3if1019.menghitungnilaimahasiswa.data.dataStore
 import com.d3if1019.menghitungnilaimahasiswa.databinding.FragmentHistoriBinding
 import com.d3if1019.menghitungnilaimahasiswa.db.NilaiDb
+import kotlinx.coroutines.launch
 
 class HistoriFragment : Fragment() {
 
@@ -22,6 +29,8 @@ class HistoriFragment : Fragment() {
 
     private lateinit var binding: FragmentHistoriBinding
     private lateinit var myAdapter: HistoriAdapter
+    private var isLinearLayoutManager = true
+    private lateinit var layoutDataStore: SettingDataStore
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,6 +39,7 @@ class HistoriFragment : Fragment() {
     ): View? {
         binding = FragmentHistoriBinding.inflate(layoutInflater,
             container, false)
+        setHasOptionsMenu(true)
         return binding.root
     }
 
@@ -41,10 +51,62 @@ class HistoriFragment : Fragment() {
             setHasFixedSize(true)
         }
 
+        layoutDataStore = SettingDataStore(requireContext().dataStore)
+        layoutDataStore.preferenceFlow.asLiveData()
+            .observe(viewLifecycleOwner, { value ->
+                isLinearLayoutManager = value
+                chooseLayout()
+                activity?.invalidateOptionsMenu()
+            })
+
         viewModel.data.observe(viewLifecycleOwner) {
             binding.emptyView.visibility = if (it.isEmpty())
                 View.VISIBLE else View.GONE
             myAdapter.submitList(it)
+        }
+    }
+
+    private fun chooseLayout() {
+        if (isLinearLayoutManager) {
+            binding.recyclerView.layoutManager =
+                LinearLayoutManager(this.requireContext())
+        } else {
+            binding.recyclerView.layoutManager =
+                GridLayoutManager(this.requireContext(), 2)
+        }
+    }
+    private fun setIcon(menuItem: MenuItem?) {
+        if (menuItem == null) return
+        menuItem.icon =
+            if (isLinearLayoutManager)
+                ContextCompat.getDrawable(requireContext(),
+                    R.drawable.ic_baseline_grid_on_24)
+            else ContextCompat.getDrawable(requireContext(),
+                R.drawable.ic_baseline_list_24)
+    }
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.layout_menu, menu)
+        val layoutButton = menu?.findItem(R.id.action_switch_layout)
+        setIcon(layoutButton)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_switch_layout -> {
+                // Sets isLinearLayoutManager to the opposite value
+                isLinearLayoutManager = !isLinearLayoutManager
+
+                lifecycleScope.launch {
+                    layoutDataStore.saveLayoutToPreferencesStore(
+                        isLinearLayoutManager, requireContext()
+                    )
+                }
+
+                // Sets layout and icon
+                chooseLayout()
+                setIcon(item)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
