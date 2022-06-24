af1e97af3cb069ca245972dd960bbbe55b7eaf7c
ismi_nuraeni <isminuraaeni@gmail.com>
2022-06-24 17:48:11 +0700

    Membuat notifikasi updater

--- app/src/main/java/com/d3if1019/menghitungnilaimahasiswa/UpdateWorker.kt
+++ app/src/main/java/com/d3if1019/menghitungnilaimahasiswa/UpdateWorker.kt
@@ -1,7 +1,11 @@
 package com.d3if1019.menghitungnilaimahasiswa
 
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 
@@ -9,8 +13,30 @@ class UpdateWorker(
     context: Context,
     workerParams: WorkerParameters
 ) : Worker(context, workerParams) {
+
+    private val notificationId = 44
+
     override fun doWork(): Result {
+        val intent = Intent(applicationContext, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or
+                Intent.FLAG_ACTIVITY_CLEAR_TASK
+        val pendingIntent = PendingIntent.getActivity(applicationContext,
+            0, intent, 0)
+
+
+        val builder = NotificationCompat.Builder(applicationContext,
+            MainActivity.CHANNEL_ID)
+            .setSmallIcon(R.mipmap.ic_launcher_round)
+            .setContentTitle(applicationContext.getString(
+                R.string.notif_title))
+            .setContentText(applicationContext.getString(
+                R.string.notif_text))
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+        val manager = NotificationManagerCompat.from(applicationContext)
+        manager.notify(notificationId, builder.build())
+
         Log.d("Worker", "Menjalankan proses background..")
+
         return Result.success()
     }
 }
